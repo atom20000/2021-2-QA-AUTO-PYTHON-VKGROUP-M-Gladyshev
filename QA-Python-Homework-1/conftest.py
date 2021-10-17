ca5aff818f357a96457d6fdc2c22bdba6bd3362a
atom20000 <0362725@mail.ru>
2021-10-17 12:57:09 +0300

    Added coment and fix chrome options

--- QA-Python-Homework-1/conftest.py
+++ QA-Python-Homework-1/conftest.py
@@ -5,19 +5,21 @@ executable_path = r'/usr/bin/chromedriver'
 Login_mytarget = 'rarebe2161@wii999.com'
 Pasword_mytarget = 'smB-g7E-rPu-TZ7'
 
-
+#Фикстура по созданию экземпляра драйвера
 @pytest.fixture(scope='function')
 def driver():
     browser = webdriver.Chrome(executable_path)
-    #browser.set_window_size(400,400)
+    browser.maximize_window()
     browser.get('https://target.my.com/')
     yield browser
     browser.close()
     
+#Фикстура по установки логина и пароля для входа
 @pytest.fixture(scope='function')
 def set_log_pwd():
     return (Login_mytarget,Pasword_mytarget)
 
+#Фикстура по установки ФИО и телефона для изменения контактной информации
 @pytest.fixture(scope='function')
 def set_contact_inf():
     return ('gdhgdl','79359-35352')
