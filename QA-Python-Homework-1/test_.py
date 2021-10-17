9d44b7940fc8ce36c1527b46c774642ffcb6b6c6
atom20000 <0362725@mail.ru>
2021-10-17 13:01:49 +0300

    Rewrite 1 test and delete unnecessary comments

---
+++ QA-Python-Homework-1/test_.py
@@ -0,0 +1,57 @@
+from locators.All_locators import *
+from Base_class import BaseCase
+import pytest
+
+class TestClass(BaseCase):
+
+#Тест по проверке входа
+
+    #@pytest.mark.skip("SKIP")
+    @pytest.mark.UI
+    def test_login(self,set_log_pwd):
+        self.login(set_log_pwd)
+        assert self.driver.current_url == 'https://target.my.com/dashboard'
+
+
+#Тест по проверке выхода
+
+    #@pytest.mark.skip("SKIP")
+    @pytest.mark.UI
+    def test_logout(self,set_log_pwd):
+        self.login(set_log_pwd)
+        self.logout()
+        assert self.driver.current_url == 'https://target.my.com/'
+
+
+#Тест по проверке изменения контактной информации
+
+    #@pytest.mark.skip("SKIP")
+    @pytest.mark.UI
+    def test_contact(self,set_log_pwd,set_contact_inf):
+        self.login(set_log_pwd)
+        self.check_button_not_hidden(Main_page.PROFILE_BUTTON, Main_page.OTHER_CENTRAL_BUTTON)
+        self.send_key(Contact_Form.FIO_FIELD,set_contact_inf[0])
+        self.send_key(Contact_Form.PHONE_FIELD,set_contact_inf[1])
+        self.click_elem(Contact_Form.BUTTON_SAVE_CHANGE)
+        self.driver.refresh()
+        assert (self.find_elem(Contact_Form.FIO_FIELD).get_attribute('value') == set_contact_inf[0] 
+        and self.find_elem(Contact_Form.PHONE_FIELD).get_attribute('value') == set_contact_inf[1])
+        self.logout()
+    
+
+#Тест по проверке перехода на вкладки Аудитория и Инструменты    
+
+    #@pytest.mark.skip("SKIP")
+    @pytest.mark.UI
+    @pytest.mark.parametrize(
+        'locator, url',
+        [
+            (Main_page.AUDITORIUM_BUTTON,'https://target.my.com/segments'),
+            (Main_page.TOOLS_BUTTON,'https://target.my.com/tools')
+        ]
+    )
+    def test_central_buttons(self, set_log_pwd, locator, url):
+        self.login(set_log_pwd)
+        self.check_button_not_hidden(locator, Main_page.OTHER_CENTRAL_BUTTON)
+        assert self.driver.current_url.find(url) !=-1
+        self.logout()
