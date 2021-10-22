15db816387e1026f6f11787fd607742ab3280a52
atom20000 <0362725@mail.ru>
2021-10-22 23:31:24 +0300

    Add an auto-authorizing fixture and returning PO main page

--- QA-Python-Homework-2/fixtures.py
+++ QA-Python-Homework-2/fixtures.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from pages.Base_page import Base_page
 from pages.Login_page import Login_page
+from pages.Main_page import Main_page
 import pytest
 
 @pytest.fixture
@@ -16,7 +17,12 @@ def cookies_login():
     browser.quit()
     return cookies
 
-
+@pytest.fixture(scope='function')
+def auto_main_page(cookies_login, driver):
+    for cookie in cookies_login:
+        driver.add_cookie(cookie)
+    driver.refresh()
+    return Main_page(driver=driver)
 #Фикстура по созданию экземпляра драйвера
 @pytest.fixture(scope='function')
 def driver():
