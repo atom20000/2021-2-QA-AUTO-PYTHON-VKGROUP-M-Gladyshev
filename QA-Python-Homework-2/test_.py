3730d7b4463a1ada28403268ef03bc39efe071c1
atom20000 <0362725@mail.ru>
2021-10-22 23:38:53 +0300

    Write a second test to check the fixture auto_login

--- QA-Python-Homework-2/test_.py
+++ QA-Python-Homework-2/test_.py
@@ -3,8 +3,14 @@ import pytest
 import time
 
 class TestClass(BaseCase):
-
+    
+    #@pytest.mark.skip('SKIP')
     @pytest.mark.UI
     def test_login(self):
-        #time.sleep(5)
+        time.sleep(5)
         assert self.driver.current_url == 'https://target.my.com/dashboard'
+
+@pytest.mark.UI
+def test_aut_log(auto_main_page):
+    time.sleep(5)
+    assert auto_main_page.driver.current_url == 'https://target.my.com/dashboard'
