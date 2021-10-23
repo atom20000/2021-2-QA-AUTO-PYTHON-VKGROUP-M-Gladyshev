048509e0c66a94a499928eb2f962013ed69d1d21
atom20000 <0362725@mail.ru>
2021-10-23 23:12:37 +0300

    Write first negative login test

--- QA-Python-Homework-2/test_.py
+++ QA-Python-Homework-2/test_.py
@@ -1,15 +1,19 @@
 from base import BaseCase
+from utils.decorators import *
 import pytest
 import time
 
 class TestClass(BaseCase):
     
-    @pytest.mark.skip('SKIP')
+    #@pytest.mark.skip('SKIP')
     @pytest.mark.UI
     def test_login(self):
-        time.sleep(5)
-        assert self.driver.current_url == 'https://target.my.com/dashboard'
-
+        login_page = self.start_page.go_to_login()
+        login_page.login('bhdg','gdfhjg')
+        assert wait(login_page.find_elem(login_page.locators.LOGIN_ERROR).is_displayed, check=True)
+    #написать второй тест с переходом на страницу ошибки
+    
+#@pytest.mark.skip('SKIP')
 @pytest.mark.UI
 def test_aut_log(auto_main_page):
     time.sleep(5)
