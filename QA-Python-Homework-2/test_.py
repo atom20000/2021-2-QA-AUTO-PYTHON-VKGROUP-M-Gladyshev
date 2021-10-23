b23e31e46c183a5a4aa9c7788832deb2a53cb0f3
atom20000 <0362725@mail.ru>
2021-10-23 21:07:07 +0300

    Skip first test

--- QA-Python-Homework-2/test_.py
+++ QA-Python-Homework-2/test_.py
@@ -4,7 +4,7 @@ import time
 
 class TestClass(BaseCase):
     
-    #@pytest.mark.skip('SKIP')
+    @pytest.mark.skip('SKIP')
     @pytest.mark.UI
     def test_login(self):
         time.sleep(5)
@@ -14,3 +14,4 @@ class TestClass(BaseCase):
 def test_aut_log(auto_main_page):
     time.sleep(5)
     assert auto_main_page.driver.current_url == 'https://target.my.com/dashboard'
+
