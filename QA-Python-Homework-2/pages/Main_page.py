70e7edc9b2d8e7e684f36447e087ce35c1cfdca9
atom20000 <0362725@mail.ru>
2021-10-24 18:44:47 +0300

    Add method go to segment page

--- QA-Python-Homework-2/pages/Main_page.py
+++ QA-Python-Homework-2/pages/Main_page.py
@@ -1,4 +1,12 @@
+from webdriver_manager import driver
 from pages.Base_page import Base_page
+from pages.Segment_page import Segment_page
+from locators.All_locators import MainPageLocators
 
 class Main_page(Base_page):
-    pass
+
+    locators = MainPageLocators()
+
+    def go_to_segment(self):
+        self.click_elem(self.locators.AUDITORIUM_BUTTON)
+        return Segment_page(driver=self.driver)
