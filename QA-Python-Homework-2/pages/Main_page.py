fa1eee56e0fdab4141e86f124dfaa5968c861a63
atom20000 <0362725@mail.ru>
2021-10-24 22:50:41 +0300

    Fix cyclic import

--- QA-Python-Homework-2/pages/Main_page.py
+++ QA-Python-Homework-2/pages/Main_page.py
@@ -1,6 +1,4 @@
-from webdriver_manager import driver
 from pages.Base_page import Base_page
-from pages.Segment_page import Segment_page
 from locators.All_locators import MainPageLocators
 
 class Main_page(Base_page):
@@ -9,4 +7,5 @@ class Main_page(Base_page):
 
     def go_to_segment(self):
         self.click_elem(self.locators.AUDITORIUM_BUTTON)
+        from pages.Segment_page import Segment_page
         return Segment_page(driver=self.driver)
