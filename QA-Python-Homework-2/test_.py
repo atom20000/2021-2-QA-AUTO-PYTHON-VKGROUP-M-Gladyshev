ce8d128759e5c3e278038623905c208e0aed76a1
atom20000 <0362725@mail.ru>
2021-10-24 15:52:04 +0300

    Add two negative test login

--- QA-Python-Homework-2/test_.py
+++ QA-Python-Homework-2/test_.py
@@ -7,13 +7,22 @@ class TestClass(BaseCase):
     
     #@pytest.mark.skip('SKIP')
     @pytest.mark.UI
-    def test_login(self):
+    def test_login_one(self):
         login_page = self.start_page.go_to_login()
-        login_page.login('bhdg','gdfhjg')
+        login_page.login('bhdg','gdfhjg') # МБ проверку на тип добавить
         assert wait(login_page.find_elem(login_page.locators.LOGIN_ERROR).is_displayed, check=True)
     #написать второй тест с переходом на страницу ошибки
     
-#@pytest.mark.skip('SKIP')
+    #@pytest.mark.skip('SKIP')
+    @pytest.mark.UI
+    def test_login_two(self):
+        login_page = self.start_page.go_to_login()
+        page = login_page.login('54534342','gdfhjg')
+        assert self.driver.current_url.find('https://account.my.com/login/') !=-1
+        assert page.find_elem(page.locators.MSG_ERROR_TITLE_LOGIN)
+        assert page.find_elem(page.locators.MSG_ERROR_TEXT_LOGIN)
+
+@pytest.mark.skip('SKIP')
 @pytest.mark.UI
 def test_aut_log(auto_main_page):
     time.sleep(5)
