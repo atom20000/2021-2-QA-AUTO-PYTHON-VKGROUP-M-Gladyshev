01c2d1e804b4780a1af9f92b31e8b3a7a86df790
atom20000 <0362725@mail.ru>
2021-10-24 15:51:32 +0300

    Add variable returned page in login method

--- QA-Python-Homework-2/pages/Login_page.py
+++ QA-Python-Homework-2/pages/Login_page.py
@@ -1,8 +1,11 @@
 from pages.Base_page import Base_page
 from pages.Main_page import Main_page
+from pages.Account_my_page import Account_my_page
 from locators.All_locators import LoginPageLocators
 
 class Login_page(Base_page):
+
+    url = 'https://target.my.com/'
 
     locators = LoginPageLocators()
 
@@ -10,4 +13,7 @@ class Login_page(Base_page):
         self.send_key(self.locators.LOG_IN_FORM, user)
         self.send_key(self.locators.PWD_FORM, passwd)
         self.click_elem(self.locators.LOG_IN_BUTTON)
-        return Main_page(self.driver)
+        if self.driver.current_url == self.url:
+            return Main_page(self.driver)
+        elif self.driver.current_url.find(Account_my_page.url) !=-1:
+            return Account_my_page(self.driver)
