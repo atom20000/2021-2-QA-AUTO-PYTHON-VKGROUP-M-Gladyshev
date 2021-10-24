0954b54fb77dfa0d285098077629c9f508ad836f
atom20000 <0362725@mail.ru>
2021-10-24 18:45:18 +0300

    Write test create segment

--- QA-Python-Homework-2/test_.py
+++ QA-Python-Homework-2/test_.py
@@ -1,11 +1,12 @@
 from base import BaseCase
 from utils.decorators import *
+from pages.Main_page import Main_page
 import pytest
 import time
 
 class TestClass(BaseCase):
     
-    #@pytest.mark.skip('SKIP')
+    @pytest.mark.skip('SKIP')
     @pytest.mark.UI
     def test_login_one(self):
         login_page = self.start_page.go_to_login()
@@ -13,7 +14,7 @@ class TestClass(BaseCase):
         assert wait(login_page.find_elem(login_page.locators.LOGIN_ERROR).is_displayed, check=True)
     #написать второй тест с переходом на страницу ошибки
     
-    #@pytest.mark.skip('SKIP')
+    @pytest.mark.skip('SKIP')
     @pytest.mark.UI
     def test_login_two(self):
         login_page = self.start_page.go_to_login()
@@ -22,9 +23,17 @@ class TestClass(BaseCase):
         assert page.find_elem(page.locators.MSG_ERROR_TITLE_LOGIN)
         assert page.find_elem(page.locators.MSG_ERROR_TEXT_LOGIN)
 
-@pytest.mark.skip('SKIP')
+#@pytest.mark.skip('SKIP')
 @pytest.mark.UI
-def test_aut_log(auto_main_page):
+def test_create_segment(auto_main_page :Main_page ):
+    segment_page = auto_main_page.go_to_segment()
+    segment_name = f'test_create_segment{time.time()}'
+    segment_page.Create_segment(segment_name)
     time.sleep(5)
-    assert auto_main_page.driver.current_url == 'https://target.my.com/dashboard'
+
+#@pytest.mark.skip('SKIP')
+#@pytest.mark.UI
+#def test_aut_log(auto_main_page):
+#    time.sleep(5)
+#    assert auto_main_page.driver.current_url == 'https://target.my.com/dashboard'
 
