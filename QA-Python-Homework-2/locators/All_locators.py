d0b6434e581e83f1633ef6cb529165e54841fb0c
atom20000 <0362725@mail.ru>
2021-10-24 18:44:13 +0300

    Add class SegmentPageLocators

--- QA-Python-Homework-2/locators/All_locators.py
+++ QA-Python-Homework-2/locators/All_locators.py
@@ -10,6 +10,13 @@ class StartPageLocators(BasicLocators):
 class AccountMyPageLocators(BasicLocators):
     MSG_ERROR_TITLE_LOGIN =(By.CLASS_NAME, 'formMsg_title')
     MSG_ERROR_TEXT_LOGIN = (By.CLASS_NAME, 'formMsg_text')
+    
+#Локаторы из формы ввода
+class LoginPageLocators(BasicLocators):
+    LOG_IN_FORM = (By.XPATH,'//input[@name="email"]')
+    PWD_FORM = (By.XPATH,'//input[@name="password"]')
+    LOG_IN_BUTTON = (By.XPATH,'//div[contains(@class,"authForm-module-button")]')
+    LOGIN_ERROR = (By.XPATH, '//div[contains(@class, "notify-module-content")]')
 
 #Локаторы с главных страниц и из центральной панели
 class MainPageLocators(BasicLocators):
@@ -18,12 +25,14 @@ class MainPageLocators(BasicLocators):
     AUDITORIUM_BUTTON = (By.XPATH ,'//li/a[contains(@href, "/segments")]')
     TOOLS_BUTTON = (By.XPATH ,'//li/a[contains(@href, "/tools")]')
 
-#Локаторы из формы ввода
-class LoginPageLocators(BasicLocators):
-    LOG_IN_FORM = (By.XPATH,'//input[@name="email"]')
-    PWD_FORM = (By.XPATH,'//input[@name="password"]')
-    LOG_IN_BUTTON = (By.XPATH,'//div[contains(@class,"authForm-module-button")]')
-    LOGIN_ERROR = (By.XPATH, '//div[contains(@class, "notify-module-content")]')
+class SegmentsPageLocators(MainPageLocators):
+    FIRST_PAGE_CREATE_BUTTON = (By.XPATH, '//a[contains(@href, "/segments/segments_list/new/")]')
+    #SEARCH_FIELD = (By.XPATH, '//div[contains(@class, "input_sources-form")]//input[contains(@class, "input__inp")]') 
+    CHECKBOX_BUTTON = (By.XPATH, '//input[contains(@class, "adding-segments-source__checkbox")]')
+    CREATE_SEGMENT_BUTTON = (By.XPATH, '//button[contains(@data-class-name, "Submit")]')
+    ADD_NAME_SEGMENT = (By.XPATH, '//div[contains(@class, "segment-name")]//input[contains(@class, "input")]')
+    ADD_SEGMENT_BUTTON = (By.XPATH, f'//div[contains(@class, "adding-segments-modal__footer")]{CREATE_SEGMENT_BUTTON[1]}')
+    LAST_PAGE_CREATE_BUTTON = (By.XPATH, f'//div[contains(@class, "create-button-wrap")]{CREATE_SEGMENT_BUTTON[1]}')
 
 #Локаторы для выхода из системы
 class LogOut_Form(BasicLocators):
