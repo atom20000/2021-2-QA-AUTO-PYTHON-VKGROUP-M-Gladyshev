36ebd4e8f0adfe3347510f48986826b90d4da479
atom20000 <0362725@mail.ru>
2021-10-28 16:54:32 +0300

    Fix create company method

--- QA-Python-Homework-2/pages/Dashboard_page.py
+++ QA-Python-Homework-2/pages/Dashboard_page.py
@@ -1,17 +1,19 @@
 from pages.Main_page import Main_page
 from locators.All_locators import DashboardPageLocators
+
 import numpy
 from PIL import Image
-from utils.decorators import wait
 import time
 import os
 
 class Dashboard_page(Main_page):
 
+    url = 'https://target.my.com/dashboard'
+
     locators = DashboardPageLocators()
 
     
-    def Create_company(self, name_company):
+    def Create_company(self, url, name_company, photo_dir):
         #if self.find_elem(self.locators.FIRST_PAGE_CREATE_BUTTON).is_displayed():
         #    self.click_elem(self.locators.FIRST_PAGE_CREATE_BUTTON)
         #elif self.find_elem(self.locators.LAST_PAGE_CREATE_BUTTON).is_displayed():
@@ -20,24 +22,23 @@ class Dashboard_page(Main_page):
         self.click_elem(self.locators.LAST_PAGE_CREATE_BUTTON) # Удалить починить If
         self.click_elem(self.locators.CHOICE_TRAFFIC_BUTTON)
         time.sleep(2)
-        self.send_key(self.locators.FIELD_LINK, 'https://mail.ru/')
+        self.send_key(self.locators.FIELD_LINK, url)
         time.sleep(2)
         self.send_key(self.locators.FIELD_NAME_COMPANY, name_company)
         self.click_elem(self.locators.FORMAT_BANNER_ADVERTISEMENT)
-        #self.Generation_Image.save(name_company)
+        photo = os.path.join(photo_dir,f'{name_company}.png')
+        self.Generation_Image.save(photo)
         self.driver.execute_script('arguments[0].style.display = "block";' , self.find_elem(self.locators.IMAGE_UPLOAD))
         time.sleep(2)
-
-        print(os.path.abspath('maxresdefault.png'))
+        #import pdb; pdb.set_trace()
+        self.send_key(self.locators.IMAGE_UPLOAD, photo)
         time.sleep(2)
-        import pdb; pdb.set_trace()
-        self.send_key(self.locators.IMAGE_UPLOAD, '/mnt/g/Репозитории/2021-2-QA-AUTO-PYTHON-VKGROUP-M-Gladyshev/maxresdefault.png')
-        time.sleep(2)
-        self.click_elem(self.locators.SAVE_IMAGE)
-        time.sleep(2)
+        #self.click_elem(self.locators.SAVE_IMAGE)
+        #time.sleep(2)
+        #import pdb; pdb.set_trace()
         self.click_elem(self.locators.BUTTON_SAVE_ADVERTISEMENT)
         self.click_elem(self.locators.BUTTON_SAVE_COMPANY)
 
     @property
     def Generation_Image(self):
-        return Image.fromarray((numpy.random.rand(100,100,3) * 255).astype('uint8')).convert('RGBA')
+        return Image.fromarray((numpy.random.rand(400,240,3) * 255).astype('uint8')).convert('RGBA')
