6b256ae5115702ab0de1019124d763cddc7c391b
atom20000 <0362725@mail.ru>
2021-10-28 21:29:33 +0300

    Create photo dir fixture

--- QA-Python-Homework-2/conftest.py
+++ QA-Python-Homework-2/conftest.py
@@ -1,1 +1,9 @@
 from fixtures import *
+
+@pytest.fixture(scope='session')
+def photo_dir():
+    photo_dir = os.path.join(os.getcwd(),'photo')
+    if os.path.exists(photo_dir):
+        shutil.rmtree(photo_dir)
+    os.mkdir(photo_dir)
+    return photo_dir
