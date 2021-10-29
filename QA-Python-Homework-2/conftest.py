78a0bf793b8adc066deb6626fc6c6665c48f77f4
atom20000 <0362725@mail.ru>
2021-10-29 22:35:34 +0300

    Fix remove dir and add screenshot, when test faild

--- QA-Python-Homework-2/conftest.py
+++ QA-Python-Homework-2/conftest.py
@@ -62,4 +62,11 @@ def save_screenshot(request,driver,temp_dir):
         screenshot = os.path.join(temp_dir, 'test.png')
         driver.get_screenshot_as_file(screenshot)
         allure.attach.file(screenshot, 'failure.png', attachment_type=allure.attachment_type.PNG)
-    
+        
+        browser_log = os.path.join(temp_dir, 'browser.log')
+        with open(browser_log, 'w') as f:
+            for i in driver.get_log('browser'):
+                f.write(f"{i['level']} - {i['source']}\n{i['message']}\n")
+
+        with open(browser_log, 'r') as f:
+            allure.attach(f.read(), 'browser.log', attachment_type=allure.attachment_type.TEXT)
