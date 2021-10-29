5e9dc4eb0bb26bc6ca75e4e2309111fdd7d0f001
atom20000 <0362725@mail.ru>
2021-10-29 21:16:59 +0300

    Add fixture create screnshot

--- QA-Python-Homework-2/conftest.py
+++ QA-Python-Homework-2/conftest.py
@@ -7,28 +7,31 @@ import allure
 @pytest.fixture(scope='function')
 def photo_dir(temp_dir):
     photo_dir = os.path.join(temp_dir,'photo')
-    #if os.path.exists(photo_dir):
-    #    shutil.rmtree(photo_dir)
+    if not os.path.exists(photo_dir):
+       os.mkdir(photo_dir)
     return photo_dir
 
 def pytest_configure(config):
     root_path = os.path.abspath(os.path.join(os.getcwd(),'data'))
+    #for attr in dir(config):
+    #    if not attr.startswith('_'):  # Если не внутренний и не служебный
+    #        print(getattr(config, attr))
     if not hasattr(config, 'workerinput'):
         if os.path.exists(root_path):
             shutil.rmtree(root_path)
         os.mkdir(root_path)
         os.mkdir(os.path.join(root_path,'allure'))
-    config.root_path = root_path
+    config.base_temp_dir = root_path
 
 @pytest.fixture(scope='function')
 def temp_dir(request):
-    temp_dir = os.path.abspath(os.path.join(request.config.root_path, request._pyfuncitem.nodeid.replace('/', '_').replace(':', '_')))
-    os.mkdir(temp_dir)
+    temp_dir = os.path.abspath(os.path.join(request.config.base_temp_dir, request._pyfuncitem.nodeid.replace('/', '_').replace(':', '_')))
+    if not os.path.exists(temp_dir):
+        os.mkdir(temp_dir)
     return temp_dir
 
 @pytest.fixture(scope='function')
 def logger(temp_dir):
-    #import pdb; pdb.set_trace()
     log_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
     log_file = os.path.join(temp_dir, 'test.log')
     log_level = logging.INFO
@@ -51,3 +54,12 @@ def logger(temp_dir):
     with open(log_file, 'r') as f:
         allure.attach(f.read(), 'test.log', attachment_type=allure.attachment_type.TEXT)
 
+@pytest.fixture(scope='function', autouse=True)
+def save_screenshot(request,driver,temp_dir):
+    failed_tests_count = request.session.testsfailed
+    yield
+    if request.session.testsfailed > failed_tests_count:
+        screenshot = os.path.join(temp_dir, 'test.png')
+        driver.get_screenshot_as_file(screenshot)
+        allure.attach.file(screenshot, 'failure.png', attachment_type=allure.attachment_type.PNG)
+    
