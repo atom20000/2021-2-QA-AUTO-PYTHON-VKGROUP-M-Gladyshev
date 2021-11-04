4f6e27eb05d5e2cbff44fd2bfe38a80b474f18b7
atom20000 <0362725@mail.ru>
2021-11-04 21:28:16 +0300

    Write create campaign method and complement other method

--- QA-Python-Homework-3/api/client.py
+++ QA-Python-Homework-3/api/client.py
@@ -1,11 +1,7 @@
-from http import cookiejar
+from urllib.parse import urljoin
+from Static_var import *
 import json
 import requests
-from urllib.parse import urljoin
-
-from requests.cookies import cookiejar_from_dict
-from requests.models import Response
-from Static_var import *
 
 class API_client():
 
@@ -16,10 +12,10 @@ class API_client():
     
     def post_login(self):
         url = 'https://auth-ac.my.com/auth'
-#        #param = {
-        #    'lang':'ru',
-        #    'nosavelogin':0
-#        #    }
+        param = {
+            'lang':'ru',
+            'nosavelogin':0
+            }
         headers = {
             'Content-Type':'application/x-www-form-urlencoded', 
             'Referer': 'https://target.my.com/'
@@ -30,13 +26,13 @@ class API_client():
             'continue': 'https://target.my.com/auth/mycom?state=target_login=1&ignore_opener=1#email',
             'failure':'https://account.my.com/login/'
         }
-        response = self.session.request('POST', url, headers=headers, data=data)
+        response = self.session.request('POST', url,params=param, headers=headers, data=data)
         self.csrf_token = [c for c in self.session.request('GET', urljoin('https://target.my.com/','csrf')).headers['Set-Cookie'].split(';') if 'csrftoken'][0].split('=')[-1]#,headers={'Referer': 'https://target.my.com/auth/mycom?state=target_login=1'})s
         return response
 
-    def get_segment(self, Id_segment=None):
+    def get_segment(self, Id_segment=None,Sort_param=None):
         if Id_segment is None:
-            url = 'https://target.my.com/api/v2/remarketing/segments.json'
+            url = f'https://target.my.com/api/v2/remarketing/segments.json{Sort_param}'
         else:
             url = f'https://target.my.com/api/v2/remarketing/segments/{Id_segment}.json'
         headers ={
@@ -47,23 +43,24 @@ class API_client():
     
     def post_create_segment(self, name):
         url = 'https://target.my.com/api/v2/remarketing/segments.json'
-#        #param = {
-        #    'fields':'relations__object_type,'
-        #    'relations__object_id,'
-        #    'relations__params,'
-        #    'relations__params__score,'
-        #    'relations__id,'
-        #    'relations_count,'
-        #    'id,'
-        #    'name,'
-        #    'pass_condition,'
-        #    'created,'
-        #    'campaign_ids,'
-        #    'users,'
-        #    'flags'
-#        #}
+        param = {
+            'fields':'relations__object_type,'
+            'relations__object_id,'
+            'relations__params,'
+            'relations__params__score,'
+            'relations__id,'
+            'relations_count,'
+            'id,'
+            'name,'
+            'pass_condition,'
+            'created,'
+            'campaign_ids,'
+            'users,'
+            'flags'
+        }
         headers = {
             'Content-Type': 'application/json',
+            'Referer': 'https://target.my.com/segments/segments_list/new/',
             'X-CSRFToken':self.csrf_token
         }
         data = json.dumps({
@@ -81,16 +78,113 @@ class API_client():
                 }
             ]
         })
-        response = self.session.request('POST', url, headers=headers,data=data)
+        response = self.session.request('POST', url,params=param, headers=headers,data=data)
         return response
     
     def delete_remove_segment(self, Id_segment):
         url = f'https://target.my.com/api/v2/remarketing/segments/{Id_segment}.json'
         headers = {
             'Content-Type': 'application/json',
+            'Referer':'https://target.my.com/segments/segments_list/new/',
             'X-CSRFToken':self.csrf_token
         }
         response = self.session.request('DELETE', url, headers=headers)
         return response
 
+    def get_campaign(self,Id_campaign=None,Sort_param=None):
+        if Id_campaign is None:
+            url = f'https://target.my.com/api/v2/campaigns.json{Sort_param}'
+        else:
+            url = f'https://target.my.com/api/v2/campaigns/{Id_campaign}.json'
+        headers ={
+            'Content-Type': 'application/json'
+        }
+        response = self.session.request('GET',url, headers=headers)
+        return response
+
+    def post_create_campaign(self, name_campaign, url_targ, photo_path):
+        url ='https://target.my.com/api/v2/campaigns.json'
+
+        headers = {
+            'Content-Type':'application/json',
+            'Referer': 'https://target.my.com/campaign/new',
+            'X-CSRFToken':self.csrf_token
+        }
+        data = json.dumps({
+            'age_restrictions':None,
+            'autobidding_mode':'second_price_mean',
+            'banners': [{
+                'content':{
+                    'image_240x400':{
+                        'id': self.session.request('POST', 'https://target.my.com/api/v2/content/static.json',
+                                headers={'X-CSRFToken':self.csrf_token},
+                                data={'width':240,'height':400},
+                                files={'file':open(photo_path,'rb'),}).json()['id']
+                    }
+                },
+                'name':'',
+                'textblocks':{},
+                'urls':{
+                    'primary':{
+                        'id':self.session.request('POST','https://target.my.com/api/v2/urls.json', 
+                                headers={'Content-Type':'application/json','X-CSRFToken':self.csrf_token},
+                                data=json.dumps({'url':url_targ})).json()['id']
+                    }
+                }
+            }],
+            'budget_limit': None,
+            'budget_limit_day': None,
+            'conversion_funnel_id': None,
+            'date_end': None,
+            'date_start': None,
+            'enable_offline_goals': False,
+            'enable_utm': True,
+            'max_price': '0',
+            'mixing': 'fastest',
+            'name':name_campaign,
+            'objective': 'traffic',
+            'package_id':961,
+            'price': '3.94',
+            'read_only': False,
+            'targetings':{
+                'age':{
+                    'age_list':[0, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75],
+                    'expand':True
+                },
+                'fulltime':{
+                    'flags': ['use_holidays_moving', 'cross_timezone'],
+                    'fri': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                    'mon': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                    'sat': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                    'sun': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                    'thu': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                    'tue': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                    'wed': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                },
+                'geo':{
+                    'regions':[188]
+                },
+                'interests': [],
+                'interests_soc_dem': [],
+                'mobile_operators': [],
+                'mobile_types':['tablets', 'smartphones'],
+                'mobile_vendors': [],
+                'pads':[102643],
+                'segments':[],
+                'sex':['male', 'female'],
+                'split_audience': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            },
+            'utm':None
+        })
+        response = self.session.request('POST', url, headers=headers, data=data)
+        return response
+
+    def delete_campaign(self, Id_campaign):
+        url = f'https://target.my.com/api/v2/campaigns/{Id_campaign}.json'
+        headers = {
+            'Content-Type': 'application/json',
+            'X-CSRFToken':self.csrf_token
+        }
+        response = self.session.request('DELETE',url,headers=headers)
+        return response
 
