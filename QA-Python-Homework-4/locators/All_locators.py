dcf61f52f5508e8dd701959c55ff664a66db4995
atom20000 <0362725@mail.ru>
2021-11-10 21:49:49 +0300

    I do not remember what I changed

--- QA-Python-Homework-4/locators/All_locators.py
+++ QA-Python-Homework-4/locators/All_locators.py
@@ -2,32 +2,33 @@ from appium.webdriver.common.mobileby import MobileBy
 
 class BasicLocators():
     #BUSTTON_ALLOW_PERMISSIOM = (MobileBy.ID, 'com.android.packageinstaller:id/permission_allow_button')
+    CAROUSEL_FACT = (MobileBy.ID, 'ru.mail.search.electroscope:id/suggests_list')
     BUTTON_POPULATION_RUSSIAN = (MobileBy.XPATH, '//android.widget.TextView[contains(@text,"население россии")]')
     RESPONSE_POPULATION_RUSSIAN = (MobileBy.ID, 'ru.mail.search.electroscope:id/item_dialog_fact_card_title')
     RESPONSE_CALCULATE = (MobileBy.XPATH, '//android.widget.TextView[contains(@text,"{}")]')
-    BUTTON_INPUT_ACTION= (MobileBy.ID, 'ru.mail.search.electroscope:id/text_input_action')
     PLAYER_TRACK_NAME = (MobileBy.ID, 'ru.mail.search.electroscope:id/player_track_name')
 
-class Main_page(BasicLocators):
+class MainpageLocators(BasicLocators):
     BUTTON_VOICE_MAIN = (MobileBy.XPATH, '//android.view.ViewGroup/android.view.View')
     BUTTON_KEYBOARD = (MobileBy.ID, 'ru.mail.search.electroscope:id/keyboard')
     BUTTON_SETINGS = (MobileBy.ID, 'ru.mail.search.electroscope:id/assistant_menu_bottom')
 
-class Main_page_with_keyboard(BasicLocators):
+class MainPagewithkeyboardLocators(BasicLocators):
     FIELD_SEARCH = (MobileBy.ID, 'ru.mail.search.electroscope:id/input_text')
     BUTTON_SEND_TEXT = (MobileBy.ID, 'ru.mail.search.electroscope:id/text_input_send')
+    BUTTON_INPUT_ACTION= (MobileBy.ID, 'ru.mail.search.electroscope:id/text_input_action')
 
-class Sitings_page():
+class SettingsPageLocators():
     BUTTON_SOURCE_NEWS = (MobileBy.ID, 'ru.mail.search.electroscope:id/user_settings_field_news_sources')
     BUTTON_ABOUT_APP = (MobileBy.ID, 'ru.mail.search.electroscope:id/user_settings_about')
-    EXIT_SETINGS = (MobileBy.XPATH, '//android.widget.LinearLayout[contains(@resource-id, "ru.mail.search.electroscope:id/user_settings_toolbar")]/android.widget.ImageButton')
+    EXIT_SETTINGS = (MobileBy.XPATH, '//android.widget.LinearLayout[contains(@resource-id, "ru.mail.search.electroscope:id/user_settings_toolbar")]/android.widget.ImageButton')
 
-class Source_news_page():
+class SourceNewsPageLocators():
     BUTTON_CHOICE_VESTE_FM = (MobileBy.XPATH,'//android.widget.FrameLayout[android.widget.TextView[contains(@text,"Вести FM")]]')
     CONFIRM_CHOICE = (MobileBy.ID, 'ru.mail.search.electroscope:id/news_sources_item_selected')
     RETURN_SETTINGS = (MobileBy.XPATH, '//android.widget.LinearLayout[contains(@resource-id, "ru.mail.search.electroscope:id/news_sources_toolbar")]/android.widget.ImageButton')
 
-class About_page():
+class AboutPageLocators():
     VERSION_APP = (MobileBy.ID, 'ru.mail.search.electroscope:id/about_version')
     COPYRIGHT_APP = (MobileBy.ID, 'ru.mail.search.electroscope:id/about_copyright')
     RETURN_SETTINGS = (MobileBy.ID, 'ru.mail.search.electroscope:id/about_toolbar_back')
