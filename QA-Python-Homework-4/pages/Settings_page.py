878b293de08cbcb5c80ab58e8f7b01c18d28d439
atom20000 <0362725@mail.ru>
2021-11-10 21:54:24 +0300

    Create settings page class

---
+++ QA-Python-Homework-4/pages/Settings_page.py
@@ -0,0 +1,12 @@
+from locators.All_locators import SettingsPageLocators
+from pages.Base_page import Base_page
+
+class Settings_page(Base_page):
+
+    locators = SettingsPageLocators()
+
+    def go_to_source_news(self):
+        pass
+    
+    def go_to_about(self):
+        pass
