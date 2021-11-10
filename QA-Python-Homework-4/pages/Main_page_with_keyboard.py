9935c8972bb8c213620bf8bcd988801cfea15738
atom20000 <0362725@mail.ru>
2021-11-10 21:53:58 +0300

    Create main page with keyboard class

---
+++ QA-Python-Homework-4/pages/Main_page_with_keyboard.py
@@ -0,0 +1,5 @@
+from locators.All_locators import MainPagewithkeyboardLocators
+from pages.Base_page import Base_page
+
+class Main_page_with_keyboard(Base_page):
+    locators = MainPagewithkeyboardLocators()
