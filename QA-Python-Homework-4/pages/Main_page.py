1522301ab74f7f4587a6be687e03e1074c4fe202
atom20000 <0362725@mail.ru>
2021-11-11 17:32:54 +0300

    Add method open settings and keyboard

--- QA-Python-Homework-4/pages/Main_page.py
+++ QA-Python-Homework-4/pages/Main_page.py
@@ -5,3 +5,13 @@ class Main_page(Base_page):
     
     locators = MainpageLocators()
 
+    def Open_Keyboard(self):
+        self.click_elem(self.locators.BUTTON_KEYBOARD)
+        from pages.Main_page_with_keyboard import Main_page_with_keyboard
+        return Main_page_with_keyboard(self.driver)
+
+    def Open_Settings(self):
+        self.click_elem(self.locators.BUTTON_SETINGS)
+        from pages.Settings_page import Settings_page
+        return Settings_page(self.driver)
+    
