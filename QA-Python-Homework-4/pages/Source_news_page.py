3b18c11c37a53e8c85f7f7c04238628ad66072c7
atom20000 <0362725@mail.ru>
2021-11-11 17:39:47 +0300

    Add method return Settings

--- QA-Python-Homework-4/pages/Source_news_page.py
+++ QA-Python-Homework-4/pages/Source_news_page.py
@@ -2,4 +2,10 @@ from locators.All_locators import SourceNewsPageLocators
 from pages.Base_page import Base_page
 
 class Source_news_page(Base_page):
+
     locators = SourceNewsPageLocators()
+    
+    def Retun_Settings(self):
+        self.click_elem(self.locators.RETURN_SETTINGS)
+        from pages.Settings_page import Settings_page
+        return Settings_page(self.driver)
