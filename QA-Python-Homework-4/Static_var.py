eb2b7855375d7310e839eac45a25bb997b936a7a
atom20000 <0362725@mail.ru>
2021-11-11 17:45:07 +0300

    Rewrite path app

--- QA-Python-Homework-4/Static_var.py
+++ QA-Python-Homework-4/Static_var.py
@@ -7,7 +7,7 @@ CAPABILITY = {
     "automationName": "Appium",
     "appPackage": "ru.mail.search.electroscope",
     "appActivity": "ru.mail.search.electroscope.ui.activity.AssistantActivity",
-    "app": os.path.abspath(os.path.join('.','stuff','Marussia_v1.50.2.apk')),
+    "app": os.path.abspath(os.path.join(os.path.dirname(__file__),'stuff','Marussia_v1.50.2.apk')),
     "orientation": "PORTRAIT",
     "autoGrantPermissions": True
 }
