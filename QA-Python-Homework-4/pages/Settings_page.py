8f93469f173cf92a268073eb08f0d7023f537c12
atom20000 <0362725@mail.ru>
2021-11-11 17:39:06 +0300

    Add method go to source news and about, exit settings

--- QA-Python-Homework-4/pages/Settings_page.py
+++ QA-Python-Homework-4/pages/Settings_page.py
@@ -1,12 +1,23 @@
-from locators.All_locators import SettingsPageLocators
 from pages.Base_page import Base_page
+from locators.All_locators import SettingsPageLocators
+from pages.Source_news_page import Source_news_page
+from pages.About_page import About_page
+from pages.Main_page import Main_page
 
 class Settings_page(Base_page):
 
     locators = SettingsPageLocators()
 
-    def go_to_source_news(self):
-        pass
+    def Go_to_Source_News(self):
+        self.scroll_by_element(method=self.swipe_up, locator_elem=self.locators.BUTTON_SOURCE_NEWS)
+        self.click_elem(self.locators.BUTTON_SOURCE_NEWS)
+        return Source_news_page(self.driver)
     
-    def go_to_about(self):
-        pass
+    def Go_to_About(self):
+        self.scroll_by_element(method=self.swipe_up, locator_elem=self.locators.BUTTON_ABOUT_APP)
+        self.click_elem(self.locators.BUTTON_ABOUT_APP)
+        return About_page(self.driver)
+    
+    def Exit_Settings(self):
+        self.click_elem(self.locators.EXIT_SETTINGS)
+        return Main_page(self.driver)
