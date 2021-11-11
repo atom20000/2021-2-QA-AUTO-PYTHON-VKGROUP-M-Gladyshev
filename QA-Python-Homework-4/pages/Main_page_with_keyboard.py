f64a40a64c87d5ab70b1af86632e5d5b514eef00
atom20000 <0362725@mail.ru>
2021-11-11 17:36:56 +0300

    Add method send text and open voice input

--- QA-Python-Homework-4/pages/Main_page_with_keyboard.py
+++ QA-Python-Homework-4/pages/Main_page_with_keyboard.py
@@ -1,5 +1,16 @@
-from locators.All_locators import MainPagewithkeyboardLocators
 from pages.Base_page import Base_page
+from locators.All_locators import MainPagewithkeyboardLocators
 
 class Main_page_with_keyboard(Base_page):
+
     locators = MainPagewithkeyboardLocators()
+
+    def Open_voice_input(self):
+        self.click_elem(self.locators.BUTTON_INPUT_ACTION)
+        from pages.Main_page import Main_page
+        return Main_page(self.driver)
+    
+    def Send_text(self,text):
+        self.send_key(self.locators.FIELD_SEARCH, text)
+        self.click_elem(self.locators.BUTTON_SEND_TEXT)
+        
