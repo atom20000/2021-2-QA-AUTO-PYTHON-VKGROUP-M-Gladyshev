0b8e7fbb0e48a3ec77551034729a156b4de3b1ed
atom20000 <0362725@mail.ru>
2021-11-12 23:35:39 +0300

    Repair init

--- QA-Python-Homework-5/python/exlist.py
+++ QA-Python-Homework-5/python/exlist.py
@@ -2,15 +2,14 @@ from typing import overload
 import re
 
 class ExList(list):
-    #@overload
-    def __init__(self) -> None:
-        super().__init__()
-    #@overload
-    def __init__(self, iterable) -> None:
-        super().__init__(iterable)
-
+    def __init__(self, iterable=None) -> None:
+        if iterable is not None:
+            super().__init__(iterable)
+        else:
+            super().__init__()
     def filter_list(self,attr,regex):
-        return ExList(filter(lambda x: re.match(rf'{regex}',getattr(x,attr)),self))
+        regex = re.compile(regex)
+        return ExList(filter(lambda x: regex.match(getattr(x,attr)),self))
 
     def mapped_list(self, attr):
         return ExList(map(lambda x: getattr(x,attr), self))
