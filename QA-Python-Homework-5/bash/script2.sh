894681612fdb6cbc073d5a65ba89203a350d9021
atom20000 <0362725@mail.ru>
2021-11-14 17:32:11 +0300

    Delete unnecessary scripts file

--- QA-Python-Homework-5/bash/script2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-exec 0< ./log/access.log
-exec 1> ./output/bash_test2.txt
-
-echo -e 'Общее количество запросов по типу\n'
-awk '{print $6}' | awk '{print substr($0, 2)}' | sort |uniq -c | awk '{print $2 " - " $1}'
-
-exec 1> /dev/tty
--- QA-Python-Homework-5/bash/script3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-exec 0< ./log/access.log
-exec 1> ./output/bash_test3.txt
-
-echo -e 'Топ 10 самых частых запросов\n'
-awk '{print $7}' | sort | uniq -c | sort -k1 -nr | awk '{print $2 "\n" $1 "\n---------------------"}' | head -n 30
-
-exec 1> /dev/tty
