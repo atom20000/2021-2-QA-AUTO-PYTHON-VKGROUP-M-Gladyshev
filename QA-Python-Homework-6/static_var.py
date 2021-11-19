f3066435e5c44167e58a191ecff59e07d5f7355d
atom20000 <0362725@mail.ru>
2021-11-19 22:05:11 +0300

    Add static variables

---
+++ QA-Python-Homework-6/static_var.py
@@ -0,0 +1,9 @@
+import os
+
+
+PATH_ACCESS_LOG_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__),'.','log','access.log'))
+USER_NAME = 'root'
+USER_PASSWORD = 'pass'
+DB_NAME = 'TEST_SQL'
+HOST = '172.22.32.1'
+PORT = '3306'
