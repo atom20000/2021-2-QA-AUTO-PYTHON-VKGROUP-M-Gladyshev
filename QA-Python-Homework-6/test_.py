adf685bf0deec4e0252b21a5b9423969a25a1468
atom20000 <0362725@mail.ru>
2021-11-20 22:13:50 +0300

    Write test upload count all request

--- QA-Python-Homework-6/test_.py
+++ QA-Python-Homework-6/test_.py
@@ -1,7 +1,11 @@
 from base import BaseCase
-
+from models.models import *
 
 class TestOne(BaseCase):
     
-    def test_create_db(self):
-        pass
+    def test_all_count_request(self):
+        self.mysql.create_table('CountAllRequest')
+        insert_row = CountAllRequest(Count_All_Request=len(self.access_log))
+        self.mysql.insert_row(insert_row)
+        response = self.mysql.select_all_rows(CountAllRequest)
+        assert response[0].Count_All_Request == insert_row.Count_All_Request
