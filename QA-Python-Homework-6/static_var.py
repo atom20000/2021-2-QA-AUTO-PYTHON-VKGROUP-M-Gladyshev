801e03a8a36251ace4695c86eb50a2af0bdd75f9
atom20000 <0362725@mail.ru>
2021-11-22 21:15:38 +0300

    Change host address

--- QA-Python-Homework-6/static_var.py
+++ QA-Python-Homework-6/static_var.py
@@ -5,5 +5,5 @@ PATH_ACCESS_LOG_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__),'.
 USER_NAME = 'root'
 USER_PASSWORD = 'pass'
 DB_NAME = 'TEST_SQL'
-HOST = '172.22.32.1'
+HOST = '127.0.0.1'
 PORT = '3306'
