b99de8d782fdeb62472dafd740bd046be14377e9
atom20000 <0362725@mail.ru>
2021-11-28 00:18:54 +0300

    Write promo test

--- QA-Python-Homework-7/test_.py
+++ QA-Python-Homework-7/test_.py
@@ -1,5 +1,10 @@
 from client.client import SocketClient
-
+from mock_server.mock import *
+import json
 
 def test_get_request(socket_client : SocketClient):
+    SURNAME_DATA['123'] = 'gshjd'
+    #import pdb; pdb.set_trace()
     print(socket_client.get_request('/get_user/123'))
+    print(socket_client.post_request('/post_user',json.dumps({'name':'hooo','surname':'hjdfld'})))
+    print(SURNAME_DATA)
