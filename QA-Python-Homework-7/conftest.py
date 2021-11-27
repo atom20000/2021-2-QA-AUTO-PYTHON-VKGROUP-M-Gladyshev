419c4357f4507ab0443e53d1f0ad7e05220c8d81
atom20000 <0362725@mail.ru>
2021-11-28 00:19:27 +0300

    Rewrite pytest_configure

--- QA-Python-Homework-7/conftest.py
+++ QA-Python-Homework-7/conftest.py
@@ -1,17 +1,16 @@
-from werkzeug.wrappers import request
-from mock_server.mock import MockServer
+from mock_server.mock import *
 from client.client import SocketClient
 import settings
 import pytest
 import requests
 
-def pytest_configure(config):
-    config.Mock = MockServer(settings.MOCK_HOST,settings.MOCK_PORT)
+def pytest_configure():
+    run_server()
     #вэйтер добавить
 
 @pytest.fixture(scope='session')
 def socket_client():
     return SocketClient(settings.MOCK_HOST,settings.MOCK_PORT)
 
-def pytest_unconfigure(config):
+def pytest_unconfigure():
     requests.get(f'http://{settings.MOCK_HOST}:{settings.MOCK_PORT}/shutdown')
