a23198e74ef87af382d08c260cb302969f08e83e
atom20000 <0362725@mail.ru>
2021-11-28 17:54:25 +0300

    Write base class for tests

--- QA-Python-Homework-7/base.py
+++ QA-Python-Homework-7/base.py
@@ -1,9 +1,18 @@
 from client.client import SocketClient
+from utils.builder import *
 import pytest
+import json
 
 
 class BaseCase():
 
-    @pytest.fixture(scope='session',autouse=True)
+    @pytest.fixture(scope='function',autouse=True)
     def setup(self, socket_client):
         self.client : SocketClient = socket_client
+        self.user_data = user_data()
+
+    def status_code_from_response(self,response):
+        return int(response[0].split()[1])
+    
+    def body_from_response(self,response):
+        return json.loads(response[-1])
