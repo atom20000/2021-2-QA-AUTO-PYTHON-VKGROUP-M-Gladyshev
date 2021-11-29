a7c7fb5361d98ef457c0952a39e27276faa71b56
atom20000 <0362725@mail.ru>
2021-11-29 21:58:41 +0300

    Add heandler response

--- QA-Python-Homework-7/base.py
+++ QA-Python-Homework-7/base.py
@@ -1,5 +1,6 @@
 from client.client import SocketClient
 from utils.builder import *
+import logging
 import pytest
 import json
 
@@ -7,12 +8,16 @@ import json
 class BaseCase():
 
     @pytest.fixture(scope='function',autouse=True)
-    def setup(self, socket_client):
+    def setup(self, socket_client, logger):
         self.client : SocketClient = socket_client
         self.user_data = user_data()
+        self.logger = logging.getLogger('client')
 
     def status_code_from_response(self,response):
         return int(response[0].split()[1])
     
     def body_from_response(self,response):
         return json.loads(response[-1])
+    
+    def hendler_response(self, response):
+        self.logger.info(f'{response[0].split()[1]} {response[1:-2]} {response[-1]}')
