a6fac0fa28fac2706f2cf48f161887ce88674226
atom20000 <0362725@mail.ru>
2021-12-06 21:16:06 +0300

    Move the mark

--- QA-Python-Homework-6/README.md
+++ QA-Python-Homework-6/README.md
@@ -1,3 +1,6 @@
 # Легкие настройки
 Путь до access.log лежит в ```static_var.py``` в переменной ```PATH_ACCESS_LOG_FILE```.  
-Тесты можно запуска с помощью марка ```-m SQL```.
+Тесты можно запуска с помощью марка ```-m SQL```.  
+Теперь переменные ```USER_NAME, USER_PASSWORD, DB_NAME, HOST, PORT```
+задаются через переменные окружения.  
+Пример запуска тестов ```USER_NAME=root USER_PASSWORD=pass DB_NAME=TEST_SQL HOST=172.30.32.1 PORT=3306 pytest -m SQL```.  
--- QA-Python-Homework-6/test_.py
+++ QA-Python-Homework-6/test_.py
@@ -2,9 +2,10 @@ from base import BaseCase
 from models.models import *
 import pytest
 
+
+@pytest.mark.SQL
 class TestOne(BaseCase):
     
-    @pytest.mark.SQL
     def test_count_all_request(self):
         self.mysql.create_table('CountAllRequest')
         insert_row = CountAllRequest(Count_All_Request=len(self.access_log))
@@ -12,7 +13,6 @@ class TestOne(BaseCase):
         response = self.mysql.select_all_rows(CountAllRequest)
         assert response[0] == insert_row
 
-    @pytest.mark.SQL
     def test_count_request_by_type(self):
         self.mysql.create_table('CountRequestByType')
         insert_rows = [CountRequestByType(Type=i[0], Count=i[1]) for i in self.access_log.mapped_list('type_request').ex_count()]
@@ -20,7 +20,6 @@ class TestOne(BaseCase):
         response = self.mysql.select_all_rows(CountRequestByType)
         assert all([i in response for i in insert_rows])
     
-    @pytest.mark.SQL
     def test_count_request_by_url(self):
         self.mysql.create_table('CountRequestByUrl')
         insert_rows = [CountRequestByUrl(Url=i[0], Count=i[1]) for i in self.access_log.mapped_list('request_url').ex_count().sorted_list(lambda x : -x[1])[:10]]
@@ -28,7 +27,6 @@ class TestOne(BaseCase):
         response = self.mysql.select_all_rows(CountRequestByUrl)
         assert all([i in response for i in insert_rows])
 
-    @pytest.mark.SQL
     def test_request_by_size_with_error400(self):
         self.mysql.create_table('RequestBySizeWithError400')
         insert_rows = [
@@ -44,7 +42,6 @@ class TestOne(BaseCase):
         response = self.mysql.select_all_rows(RequestBySizeWithError400)
         assert all([i in response for i in insert_rows])
     
-    @pytest.mark.SQL
     def test_count_request_by_ip_with_error500(self):
         self.mysql.create_table('CountRequestByIpWithError500')
         insert_rows = [
