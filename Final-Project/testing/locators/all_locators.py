90fd364e83d1cba63f4135898462fbc8c38e98f5
atom20000 <0362725@mail.ru>
2021-12-15 15:35:53 +0300

    Complete main page locators

--- Final-Project/testing/locators/all_locators.py
+++ Final-Project/testing/locators/all_locators.py
@@ -19,4 +19,8 @@ class MainPageLocators():
     LOGOUT_BUTTON = (By.XPATH, '//a[contains(@href,"/logout")]')
     LOGIN_NAME = (By.XPATH, '//div[contains(@id,"login-name")]//li[contains(text(),"Logged as {}")]')
     VK_ID = (By.XPATH, '//div[contains(@id,"login-name")]//li[contains(text(),"VK ID: {}")]')
+    NAVBAR_BUTTON = (By.XPATH, '//li[contains(@class, "uk-parent")]/a[contains(text(), "{}")]')
+    BUG_BUTTON = (By.XPATH, '//a[contains(@class,"uk-navbar-brand")]')
+    NAVBAR_DROPLIST_BUTTON = (By.XPATH, '//div[contains(@class,"uk-dropdown")]//a[contains(text(), "{}")]')
+    OVERLAY_BUTTON = (By.XPATH, '//div[contains(text(),"{}")]/following-sibling::figure')
     
