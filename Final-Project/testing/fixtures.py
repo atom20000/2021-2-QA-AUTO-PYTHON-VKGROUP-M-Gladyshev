1af20ace3c8806dbf2de860efcaeaff5c0f2356f
atom20000 <0362725@mail.ru>
2021-12-17 19:30:05 +0300

    Write fixtures for tests

--- Final-Project/testing/fixtures.py
+++ Final-Project/testing/fixtures.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from pages.login_page import LoginPage 
 from pages.main_page import MainPage
-from clients.clent_db.client_mysql import MySQLORMClient
+from clients.client_db.client_mysql import MySQLORMClient
 from clients.client_app import AppClient
 from clients.client_mock import MockClient
 import logging
@@ -16,10 +16,12 @@ def start_page(driver):
     return LoginPage(driver=driver)
 
 @pytest.fixture(scope='session')
-def cookies_login():
+def cookies_login(mysql_orm_client):
     browser = get_driver()
-    browser.get('QA_myapp:8080/login') 
-    LoginPage(browser).login()
+    browser.get('http://qa_myapp_proxy:8082/login') 
+    user = mysql_orm_client.generate_user()
+    mysql_orm_client.insert_row(user)
+    LoginPage(browser).login(username=user.username, password=user.password)
     cookies = browser.get_cookies()
     browser.quit()
     
@@ -28,7 +30,10 @@ def cookies_login():
 @pytest.fixture(scope='function')
 def auto_main_page(cookies_login, driver):
     for cookie in cookies_login:
-        driver.add_cookie(cookie)
+        driver.add_cookie({
+            'name': cookie.get('name'),
+            'value': cookie.get('value')
+        })
     driver.refresh()
     with allure.step('Cookies added'):
         return MainPage(driver=driver)
@@ -38,7 +43,7 @@ def auto_main_page(cookies_login, driver):
 def driver():
     browser = get_driver()
     #проверить доступ к app
-    browser.get('http://QA_myapp:8080')
+    browser.get('http://qa_myapp_proxy:8082/login')
     with allure.step('Create driver'):
         yield browser
     with allure.step('Quite driver'):
@@ -51,7 +56,6 @@ def get_driver():
         'browserName':'chrome',
         'version':'96.0'
     }
-    #проверить доступ к селеноид
     browser = webdriver.Remote(f'http://selenoid:4444/wd/hub',options=options, desired_capabilities=capabilites)
     browser.maximize_window()
     return browser
@@ -65,9 +69,9 @@ def mysql_orm_client():
 
 @pytest.fixture(scope='session')
 def api_client():
-    return AppClient('http://QA_myapp:8080')
+    return AppClient('http://qa_myapp_proxy:8082')
 
 @pytest.fixture(scope='session')
 def mock_client():
-    return MockClient('http://VK_mock:8080')
+    return MockClient('http://vk_mock:8080')
 
